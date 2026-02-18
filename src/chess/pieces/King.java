6cfc2717814c7bd1db5f4d0a8fabed5a88cb09d7
Gabs-jg <gabriel19joao@gmail.com>
2026-02-18 12:23:52 -0300

    Knight

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -72,7 +72,6 @@ public class King extends ChessPiece {
         if (getBoard().positionExists(p) && canMove(p)) {
             mat[p.getRow()][p.getColumn()] = true;
         }
-
         return mat;
     }
 }
